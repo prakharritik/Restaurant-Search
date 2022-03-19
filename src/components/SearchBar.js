2c29ce6670d3da2942d6ec858f3235813f14abea
prakharritik <60786008+prakharritik@users.noreply.github.com>
2022-03-19 12:43:54 +0530

    Initial Commit.

---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SearchBar = () => {
+  return (
+    <View style={styles.background}>
+      <Text>SearchBar</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#F0EEEE",
+  },
+});
+
+export default SearchBar;
