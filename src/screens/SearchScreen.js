63f798d86333ba17f9e08720e88f26695eb9cd07
prakharritik <60786008+prakharritik@users.noreply.github.com>
2022-03-19 22:09:23 +0530

    Add the search API functionality.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
+import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [searchApi, results, errorMessage] = useResults();
+
   return (
     <View style={styles.background}>
-      <SearchBar
-        term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log("Submitted")}
-      />
-      <Text>SearchScreen </Text>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results.</Text>
     </View>
   );
 };
